d1a63fdce2e7f3361be26621c389b7f2bd89b64f
terryyn <yeyunong@hotmail.com>
2018-04-01 11:45:06 -0700

    Update server.py

--- server.py
+++ server.py
@@ -6,26 +6,6 @@ import operator
 import sys
 
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "eGl3ZWk="
-
-@app.route('/api/function_result')
-def function_result():
-    datastring = request.args.get('param')
-    datadict = {}
-    datalist = datastring.split()
-    number = int(datalist[1])
-    datadict['number'] = number
-    operatorString = datalist[2]
-    for i in range (0,number-1):
-        datadict['operator%d'%(i)] = operatorString[i]
-    for x in range (0,number):
-        datadict['object%d'%(x)] = datastring[3+x]
-    return get_function(json.dumps(datadict))
-
 
 def get_function(data):
     ops = {'+': operator.add, '-': operator.sub ,'*':operator.mul}
@@ -45,25 +25,25 @@ def get_function(data):
         typex = temp[:location]
         valuex = temp[location+1:]
         valuedict['object%d'%(x)] = (typex,valuex)
-        if(typex != 'str'):
+        if(typex != 'string'):
             allstring = False
         else:
             nostring = False
-        if(typex == 'float'):
+        if(typex == 'double'):
             existfloat = True
     for y in range(0,number-1):
         operatorlist.append(datadict["operator%d"%(y)])
         if(datadict["operator%d"%(y)] != '+'):
             allplus = False
     if not nostring and not allstring:
-        return 'error: cannot do operation between string and int/float'
+        return 'error: cannot do operation between string and int/double'
     elif allstring and not allplus:
         return 'error: cannot do the operation on string'
     elif allstring and allplus:
         for i in range(0,number):
             result = result + valuedict['object%s'%(i)][1]
-        return 'str:%s'%(result)
-    else:
+        return 'string:%s'%(result)
+    else:   
         operator1 = operatorlist[0]
         result = ops[operator1](float(valuedict['object0'][1]),float(valuedict['object1'][1]))
         for i in range(2,number):
@@ -73,8 +53,32 @@ def get_function(data):
             result = int(result)
             return 'int:%d'%(result)
         result  =str(result)
-    return 'float:%s'%(result)
+    return 'double:%s'%(result)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "eGl3ZWk="
+
+@app.route('/api/function_result',methods = ['GET','POST'])
+def function_result():
+    temp = request.get_json()
+    datastring = temp['param']
+    datadict = {}
+    datalist = datastring.split()
+    number = int(datalist[1])
+    datadict['number'] = number
+    operatorString = datalist[2]
+    for i in range (0,number-1):
+        datadict['operator%d'%(i)] = operatorString[i]
+    for x in range (0,number):
+        datadict['object%d'%(x)] = datalist[3+x]
+    return jsonify(get_function(json.dumps(datadict)))
+
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=5000)
+
+
 
